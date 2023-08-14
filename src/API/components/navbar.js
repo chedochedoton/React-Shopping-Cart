06ab74d3f3ef4758c2220a2dde02b663370208fc
chedochedoton <naro.billa@gmail.com>
2023-08-14 11:43:33 +0100

    Search changé à Recherche

--- src/API/components/navbar.js
+++ src/API/components/navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
         <>
         <header className="flex flex-row flex-grow items-center">
             <div className="flex flex-row flex-grow relative items-center space-x-2">
-                <input className="px-4 py-2 w-66 rounded-full text-sm text-grey-600 outline-none" type="text" placeholder="Search"/>
+                <input className="px-4 py-2 w-66 rounded-full text-sm text-grey-600 outline-none" type="text" placeholder="Recherche"/>
 
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 top-0 left-10 text-grey-500 bg-white p-3 w-10 h-10 rounded-full cursor-pointer hover:bg-blue-400 hover:text-white transition duration-700 ease-in-out" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 211-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
